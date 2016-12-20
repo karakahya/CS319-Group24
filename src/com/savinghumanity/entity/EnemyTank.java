d8aa7e3ca460a1ec003ec17d4bda9369c9255053
erdemkoo <karaosmanoglu95@gmail.com>
2016-12-20 17:05:31 +0300

    Update EnemyTank.java

--- src/com/savinghumanity/entity/EnemyTank.java
+++ src/com/savinghumanity/entity/EnemyTank.java
@@ -3,21 +3,64 @@ package com.savinghumanity.entity;
 import java.awt.image.BufferedImage;
 
 
-public class EnemyTank extends Tank {
+public class EnemyTank extends Tank{
 
-	private int type;
-
-	/**
-	 * 
-	 * @param xPos
-	 * @param yPos
-	 * @param type
-	 */
-	public EnemyTank(int xPos, int yPos, int type) {
-		super(xPos,yPos);
-		this.type = type;
-		// TODO - implement EnemyTank.EnemyTank
+	public int type;
 	
+	public EnemyTank(int corX, int corY, boolean isalive, int typeEnemy){
+		posX = corX;
+		posY = corY;
+		isAlive = isalive;
+		type = typeEnemy;
+		
+		if(type==1){
+			speed = 10;
+			fireRange = 10;
+			damage = 10;
+			health = 10;
+			speedX = 10;
+			speedY = 10;
+			
+			
+		}
+		else if(type==2){
+			speed = 20;
+			fireRange = 20;
+			damage = 20;
+			health = 20;
+			speedX = 20;
+			speedY = 20;
+			
+			
+		}
+		
+		else if(type==3){
+			speed = 20;
+			fireRange = 20;
+			damage = 20;
+			health = 20;
+			speedX = 20;
+			speedY = 20;
+			
+	
+		}
+		
+		
+		
+		}
+	
+	public void findaPath(){
+		
+		
 	}
-
+	
+	public void checkDestination(EnemyTank et, PlayerTank pt){
+		if(et.getPosX()== pt.getPosX() || et.getPosY() == pt.getPosY())
+			et.fire();
+		
+		
+		
+	}
+	
+	
 }
