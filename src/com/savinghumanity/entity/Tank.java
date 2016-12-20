6b94fb41cd0e5f9475f9a863855ebd08e6281b7a
erdemkoo <karaosmanoglu95@gmail.com>
2016-12-20 17:04:36 +0300

    Update Tank.java

--- src/com/savinghumanity/entity/Tank.java
+++ src/com/savinghumanity/entity/Tank.java
@@ -2,28 +2,66 @@ package com.savinghumanity.entity;
 
 import java.awt.image.BufferedImage;
 
-public abstract class Tank extends GameObject {
-
-	private int speed;
-	private int fireRange;
-	private int damage;
-	private int health;
-	private int speedX;
-	private int speedY;
-
-	public Tank(int posX,int posY) {
-		super(posX,posY);
-		// TODO - implement Tank.Tank
+public class Tank extends GameObject {
+	public int speed;
+	public int fireRange;
+	public int damage;
+	public int health;
+	public int speedX;
+	public int speedY;
+	
+	public Tank(int corX, int corY,  int firstSpeed, int firstRange, int firstDamage, int firstHealth, int firstX, int firstY) {
+		super(corX, corY);
+		speed = firstSpeed;
+		fireRange = firstRange;
+		damage = firstDamage;
+		health = firstHealth;
+		speedX = firstX;
+		speedY = firstY;
+		isAlive = true;
+	}
+	
+	public void setSpeed(int value){
+		
+		speed = value;
 	}
 
-	public void update() {
-		// TODO - implement Tank.update
-		throw new UnsupportedOperationException();
+	public void setFireRange(int value){
+		
+		fireRange = value;
 	}
-
-	public void fire() {
-		// TODO - implement Tank.fire
-		throw new UnsupportedOperationException();
+	
+	public void setDamage(int value){
+		
+		damage = value;
 	}
-
+	
+	public void setHealth(int value){
+		
+		health = value;
+	}
+	
+	public void setSpeedX(int value){
+		
+		speedX = value;
+	}
+	
+	public void setSpeedY(int value){
+		
+		speedY = value;
+	}
+	
+	
+	
+	
+	public void update(){
+		
+		
+	}
+	
+	public void fire(){
+		Bullet bull = new Bullet();
+		
+		
+	}
 }
