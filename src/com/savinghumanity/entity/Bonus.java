14ef8d6e4ea730aee3422ec987ad4f221fd699ab
erdemkoo <karaosmanoglu95@gmail.com>
2016-12-20 17:11:35 +0300

    Update Bonus.java

--- src/com/savinghumanity/entity/Bonus.java
+++ src/com/savinghumanity/entity/Bonus.java
@@ -3,14 +3,16 @@ package com.savinghumanity.entity;
 import java.awt.image.BufferedImage;
 
 public abstract class Bonus extends GameObject {
-
-	public Bonus(int posX, int posY) {
-		super(posX, posY);
-		// TODO Auto-generated constructor stub
+	public int durationTime;
+	public long startingTime;
+	
+	public Bonus(int corX, int corY, boolean isalive, int duration, int start) {
+		super(corX, corY, isalive);
+		durationTime = duration;
+		startingTime = start;
+		
+		
 	}
-
-	private int durationTime;
-	private long startingTime;
 
 	public abstract void effect();
 
