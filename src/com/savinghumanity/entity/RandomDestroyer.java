3f77da85e8b9c82cdd50c823c852bd421721bc87
erdemkoo <karaosmanoglu95@gmail.com>
2016-12-20 17:16:39 +0300

    Update RandomDestroyer.java

--- src/com/savinghumanity/entity/RandomDestroyer.java
+++ src/com/savinghumanity/entity/RandomDestroyer.java
@@ -1,19 +1,20 @@
 package com.savinghumanity.entity;
-
+import java.util.Random;
 import java.awt.image.BufferedImage;
 
 public class RandomDestroyer extends Bonus {
 
-	public RandomDestroyer(int posX, int posY) {
-		super(posX, posY);
-		// TODO Auto-generated constructor stub
+	public RandomDestroyer(int corX, int corY, boolean isalive, int duration, int start){
+		
+		super(corX,corY,isalive,duration,start);
+}
+	
+	public void effect(int size, EnemyTank[] army){
+		Random random = new Random();
+		int lucky = random.nextInt(size);
+		
+		
 	}
-
-	public void effect() {
-		// TODO - implement RandomDestroyer.effect
-		throw new UnsupportedOperationException();
-	}
-
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
